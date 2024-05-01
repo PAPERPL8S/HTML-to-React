075a5510e62de19978a10939437d55f5393a6774
Andre Ansah <145819692+PAPERPL8S@users.noreply.github.com>
2024-04-30 22:41:52 -0500

    added styles to center sections

--- src/components/CharacterCards/_components/CharacterCard.jsx
+++ src/components/CharacterCards/_components/CharacterCard.jsx
@@ -1,12 +1,12 @@
 const CharacterCard = ({ character }) => {
-  const { name, nickName, background, imageURL } = character;
+  const { name, nickName, background, imageUrl } = character;
   return (
     <div className="card">
       <div className="card-titles">
         <h3>{name}</h3>
         {nickName && <h4>{nickName}</h4>}
       </div>
-      <img src={imageURL} alt={name} />
+      <img src={imageUrl} alt={name} />
       <p>{background}</p>
     </div>
   );
